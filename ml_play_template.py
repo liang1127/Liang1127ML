930bdaf4727596f8227629ae940423c651a26028
liang1127 <39379198+liang1127@users.noreply.github.com>
2020-11-25 16:33:49 +0800

    HW2

--- ml_play_template.py
+++ ml_play_template.py
@@ -5,72 +5,76 @@ import games.arkanoid.communication as comm
 from games.arkanoid.communication import ( \
     SceneInfo, GameInstruction, GameStatus, PlatformAction
 )
+import pickle
+import numpy as np
 
 def ml_loop():
-    """The main loop of the machine learning process
-    This loop is run in a separate process, and communicates with the game process.
-    Note that the game process won't wait for the ml process to generate the
-    GameInstruction. It is possible that the frame of the GameInstruction
-    is behind of the current frame in the game process. Try to decrease the fps
-    to avoid this situation.
-    """
-
-    # === Here is the execution order of the loop === #
-    # 1. Put the initialization code here.
-
-    # 2. Inform the game process that ml process is ready before start the loop.
-    ball_position_history = []
+    mode = "TestTrain" # TestTrain RuleBase
+    predictFunction = "knn" #svm
+    aid = 0
+    past_ball_position = []
+    ball_down = False
     comm.ml_ready()
 
-    # 3. Start an endless loop.
-    while True:
-        # 3.1. Receive the scene information sent from the game process.
-        scene_info = comm.get_scene_info()
-        ball_position_history.append(scene_info.ball)
-        platform_center_x = scene_info.platform[0] + 20
-        if len(ball_position_history) == 1:
-            ball_going_down = 0
-            
-        elif ball_position_history[-1][1]-ball_position_history[-2][1] > 0:
-            ball_going_down = 1
-            vy = ball_position_history[-1][1]-ball_position_history[-2][1]
-            vx = ball_position_history[-1][0]-ball_position_history[-2][0]
+    if mode == "RuleBase":
+        while True:
+            scene_info = comm.get_scene_info()
 
-        else: 
-            ball_going_down = 0
-        # 3.2. If the game is over or passed, the game process will reset
-        #      the scene and wait for ml process doing resetting job.
-        if scene_info.status == GameStatus.GAME_OVER or \
-            scene_info.status == GameStatus.GAME_PASS:
-            # Do some stuff if needed
+            if scene_info.status == GameStatus.GAME_OVER or \
+                scene_info.status == GameStatus.GAME_PASS:
+                comm.ml_ready()
+                continue
+            now_ball_position = scene_info.ball
 
-            # 3.2.1. Inform the game process that ml process is ready
-            comm.ml_ready()
-            continue
+            if len(past_ball_position) == 0:
+                past_ball_position = now_ball_position
+            else:
+                if (now_ball_position[1] - past_ball_position[1]) > 0:
+                    ball_down = True
+                else:
+                    ball_down = False
+            now_platform_positionX = scene_info.platform[0] + 20
 
-        # 3.3. Put the code here to handle the scene information
-        if ball_going_down == 1 and ball_position_history[-1][1] >= 50:
-            ball_destination = ball_position_history[-1][0]+ ((395-ball_position_history[-1][1])/vy)*vx
+            if ball_down == True and now_ball_position[1] > 200:
+                m = (now_ball_position[1] - past_ball_position[1]) / (now_ball_position[0] - past_ball_position[0])
+                aid = now_ball_position[0] - ((now_ball_position[1] - 395) / m)
+                if aid < 0:
+                    aid = -aid
+                elif aid > 200:
+                    aid = 200 - (aid - 200)
+            else:
+                aid = 100
 
-            if ball_destination >= 195:
-                ball_destination = 195 - (ball_destination - 195) % 195
-          
-            elif ball_destination <= 0 :
-                ball_destination = ball_destination % 195
-                ball_destination = -platform_center_x
-        else:
-            ball_destination = platform_center_x
-        
-
-        # 3.4. Send the instruction for this frame to the game process
-        if(ball_going_down == 1 and ball_position_history[-1][1] >= 280):
-            if platform_center_x > ball_destination:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            elif platform_center_x < ball_destination:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)            
-           
-        elif(ball_going_down == 0):
-            if(platform_center_x < 100):
+            if aid > now_platform_positionX:
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            elif(platform_center_x > 100):
+            if aid < now_platform_positionX:
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            if aid == now_platform_positionX:
+                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+            past_ball_position = now_ball_position
+
+    if mode == "TestTrain":   
+        filename = predictFunction + ".sav"
+        model = pickle.load(open(filename, 'rb')) 
+        
+        while True:
+            scene_info = comm.get_scene_info()
+            now_ball_position = scene_info.ball
+
+            if len(past_ball_position) != 0:
+                inp_temp = np.array([past_ball_position[0], past_ball_position[1], now_ball_position[0], now_ball_position[1], scene_info.platform[0]])
+                input = inp_temp[np.newaxis, :]
+                
+                if scene_info.status == GameStatus.GAME_OVER or \
+                    scene_info.status == GameStatus.GAME_PASS:
+                    comm.ml_ready()
+                    continue
+                move = model.predict(input)
+
+                if move < 0:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                elif move > 0:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                else:
+                    comm.send_instruction(scene_info.frame, PlatformAction.NONE)    
+            past_ball_position = now_ball_position
